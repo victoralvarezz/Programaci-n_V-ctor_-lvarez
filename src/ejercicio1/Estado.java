c0820d8b1fcddefef7dfb31f09c21f2b831975a7
victor alvarez <victoralvgon2005@gmail.com>
2026-03-08 23:18:47 +0100

    he añdido mas estados

---
+++ src/ejercicio1/Estado.java
@@ -0,0 +1,16 @@
+package ejercicio1;
+
+public abstract class Estado {
+	protected String nombre;
+	protected int turnosRestantes;
+	protected int potenciaPorTurno;
+
+	public Estado(String nombre, int potenciaPorTurno, int turnosRestantes) {
+		this.nombre = nombre;
+		this.potenciaPorTurno = potenciaPorTurno;
+		this.turnosRestantes = turnosRestantes;
+	}
+
+	// cada estado aplica su efecto cada una el suyo
+	public abstract void aplicarEfecto(persona.Personaje objetivo);
+}
