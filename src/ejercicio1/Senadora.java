05d51dc0ecf7f0230fa5ca3929cc1b5aef8a2bb7
victor alvarez <victoralvgon2005@gmail.com>
2026-04-07 10:42:44 +0200

    trabajo terminado

--- src/ejercicio1/Senadora.java
+++ src/ejercicio1/Senadora.java
@@ -1,21 +1,44 @@
 package ejercicio1;
 
-public class Senadora extends Estado {
+/**
+ * Clase abstracta que representa un estado persistente aplicado a un personaje.
+ * Un estado tiene una duracion en turnos y aplica un efecto (daño o curacion)
+ * cada ronda hasta que expira. Las subclases concretas son Quemaduras, Veneno y
+ * Senadora (Renovar).
+ *
+ * @author Victor
+ * @version 1.0
+ */
+public abstract class Estado {
 
-	// RENOVAR = curación por turnos (la usa el Sanador)
-	public Senadora() {
-		super("Renovar", 10, 2); // cura 10 durante 2 turnos
+	/** Nombre del estado que se muestra en combate. */
+	protected String nombre;
+
+	/** Turnos que le quedan al estado antes de expirar. */
+	protected int turnosRestantes;
+
+	/** Cantidad de daño o curacion que aplica por turno. */
+	protected int potenciaPorTurno;
+
+	/**
+	 * Constructor de Estado.
+	 *
+	 * @param nombre           nombre del estado
+	 * @param potenciaPorTurno cantidad de daño o curacion por turno
+	 * @param turnosRestantes  duracion del estado en turnos
+	 */
+	public Estado(String nombre, int potenciaPorTurno, int turnosRestantes) {
+		this.nombre = nombre;
+		this.potenciaPorTurno = potenciaPorTurno;
+		this.turnosRestantes = turnosRestantes;
 	}
 
-	@Override
-	public void aplicarEfecto(persona.Personaje objetivo) {
-		if (!objetivo.estaVivo())
-			return;
-		objetivo.vida += potenciaPorTurno;
-		if (objetivo.vida > objetivo.vidaMax)
-			objetivo.vida = objetivo.vidaMax;
-		System.out.println("  [Renovar] " + objetivo.nombre + " recupera " + potenciaPorTurno + " de vida | Vida: "
-				+ objetivo.vida);
-		turnosRestantes--;
-	}
+	/**
+	 * Aplica el efecto del estado sobre el personaje objetivo. Cada subclase define
+	 * si el efecto es daño o curacion. Tambien decrementa turnosRestantes en cada
+	 * llamada.
+	 *
+	 * @param objetivo el personaje sobre el que se aplica el efecto
+	 */
+	public abstract void aplicarEfecto(persona.Personaje objetivo);
 }
