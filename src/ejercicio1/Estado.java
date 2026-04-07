3378ba0e72c8b91bb53a0d697f131da93372cb94
victor alvarez <victoralvgon2005@gmail.com>
2026-04-07 09:39:10 +0200

    Trabajo terminado

--- src/ejercicio1/Estado.java
+++ src/ejercicio1/Estado.java
@@ -1,16 +1,44 @@
 package ejercicio1;
 
+/**
+ * Clase abstracta que representa un estado persistente aplicado a un personaje.
+ * Un estado tiene una duracion en turnos y aplica un efecto (daño o curacion)
+ * cada ronda hasta que expira. Las subclases concretas son Quemaduras, Veneno y
+ * Senadora (Renovar).
+ *
+ * @author Victor
+ * @version 1.0
+ */
 public abstract class Estado {
+
+	/** Nombre del estado que se muestra en combate. */
 	protected String nombre;
+
+	/** Turnos que le quedan al estado antes de expirar. */
 	protected int turnosRestantes;
+
+	/** Cantidad de daño o curacion que aplica por turno. */
 	protected int potenciaPorTurno;
 
+	/**
+	 * Constructor de Estado.
+	 *
+	 * @param nombre           nombre del estado
+	 * @param potenciaPorTurno cantidad de daño o curacion por turno
+	 * @param turnosRestantes  duracion del estado en turnos
+	 */
 	public Estado(String nombre, int potenciaPorTurno, int turnosRestantes) {
 		this.nombre = nombre;
 		this.potenciaPorTurno = potenciaPorTurno;
 		this.turnosRestantes = turnosRestantes;
 	}
 
-	// cada estado aplica su efecto cada una el suyo
+	/**
+	 * Aplica el efecto del estado sobre el personaje objetivo. Cada subclase define
+	 * si el efecto es daño o curacion. Tambien decrementa turnosRestantes en cada
+	 * llamada.
+	 *
+	 * @param objetivo el personaje sobre el que se aplica el efecto
+	 */
 	public abstract void aplicarEfecto(persona.Personaje objetivo);
 }
--- src/ejercicio1/main.java
+++ src/ejercicio1/main.java
@@ -1,72 +1,408 @@
 package ejercicio1;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Clase principal que controla el flujo del juego RPG por turnos. Gestiona el
+ * menu inicial, la creacion de equipos, el bucle de combate y el resumen final
+ * de la partida. Usa un HashMap para el catalogo de personajes disponibles.
+ *
+ * @author Victor
+ * @version 1.0
+ */
 public class main {
 
+	/** Scanner para leer opciones del jugador por consola. */
+	static Scanner sc = new Scanner(System.in);
+
+	/** Mapa que relaciona un numero con el nombre del tipo de personaje. */
+	static Map<Integer, String> menuPersonajes = new HashMap<Integer, String>();
+
+	static {
+		menuPersonajes.put(1, "Jedi");
+		menuPersonajes.put(2, "Sith");
+		menuPersonajes.put(3, "Soldado Rebelde");
+		menuPersonajes.put(4, "Soldado Imperial");
+		menuPersonajes.put(5, "Cazarrecompensas");
+		menuPersonajes.put(6, "Sanador");
+	}
+
+	/**
+	 * Punto de entrada del programa. Muestra el menu, configura los equipos y
+	 * ejecuta el bucle de combate.
+	 *
+	 * @param args argumentos de la linea de comandos (no se usan)
+	 */
 	public static void main(String[] args) {
 
-		// equipos de momento fijos, luego lo hago con scanner
-		persona.Personaje[] rebeldes = { new persona.Jedi("Yoda"), new persona.SoldadoRebelde("Han Solo"),
-				new persona.Sanador("Leia") };
+		System.out.println("=== RPG POR TURNOS ===");
+		System.out.println("1) Iniciar combate");
+		System.out.println("2) Salir");
 
-		persona.Personaje[] imperio = { new persona.Sith("Darth Vader"), new persona.SoldadoImperial("Stormtrooper"),
-				new persona.Cazarrecompensas("Boba Fett") };
+		int opcionInicio = leerEntero(1, 2);
+
+		if (opcionInicio == 2) {
+			System.out.println("Hasta luego.");
+			sc.close();
+			return;
+		}
+
+		System.out.println("\nElige modo:");
+		System.out.println("1) Combate automatico");
+		System.out.println("2) Elegir mi equipo y jugar manual");
+
+		int modo = leerEntero(1, 2);
+
+		persona.Personaje[] equipoA;
+		persona.Personaje[] equipoB;
+
+		if (modo == 1) {
+			equipoA = new persona.Personaje[] { new persona.Jedi("Yoda"), new persona.SoldadoRebelde("Han Solo"),
+					new persona.Sanador("Leia") };
+			equipoB = new persona.Personaje[] { new persona.Sith("Darth Vader"),
+					new persona.SoldadoImperial("Stormtrooper"), new persona.Cazarrecompensas("Boba Fett") };
+			System.out.println("\nEquipo A: Yoda / Han Solo / Leia");
+			System.out.println("Equipo B: Darth Vader / Stormtrooper / Boba Fett");
+		} else {
+			System.out.println("\n=== ELIGE TU EQUIPO ===");
+			equipoA = elegirEquipo("Tu equipo");
+			equipoB = new persona.Personaje[] { new persona.Sith("Darth Vader"),
+					new persona.SoldadoImperial("Stormtrooper"), new persona.Cazarrecompensas("Boba Fett") };
+			System.out.println("\nEquipo enemigo: Darth Vader / Stormtrooper / Boba Fett");
+		}
 
 		int ronda = 1;
-		System.out.println("=== COMBATE INICIADO ===\n");
+		System.out.println("\n=== COMBATE INICIADO ===");
 
-		while (hayVivos(rebeldes) && hayVivos(imperio)) {
+		while (hayVivos(equipoA) && hayVivos(equipoB)) {
+
 			System.out.println("\n===== RONDA " + ronda + " =====");
+			sleep(1700);
 
-			// primero atacan los rebeldes
-			System.out.println("\n-- Turno Rebeldes --");
-			for (int i = 0; i < rebeldes.length; i++) {
-				if (rebeldes[i] != null && rebeldes[i].estaVivo()) {
-					rebeldes[i].hacerTurno(imperio, rebeldes);
+			System.out.println("\n-- Equipo A --");
+			for (persona.Personaje p : equipoA)
+				if (p != null)
+					mostrarPersonaje(p);
+
+			System.out.println("\n-- Equipo B --");
+			for (persona.Personaje p : equipoB)
+				if (p != null)
+					mostrarPersonaje(p);
+
+			System.out.println("\n-- Turno Equipo A --");
+			for (persona.Personaje p : equipoA) {
+				if (p != null && p.estaVivo()) {
+					if (modo == 1)
+						p.hacerTurno(equipoB, equipoA);
+					else
+						turnoJugador(p, equipoB, equipoA);
+					sleep(1700);
 				}
 			}
 
-			// luego el imperio
-			System.out.println("\n-- Turno Imperio --");
-			for (int i = 0; i < imperio.length; i++) {
-				if (imperio[i] != null && imperio[i].estaVivo()) {
-					imperio[i].hacerTurno(rebeldes, imperio);
+			if (!hayVivos(equipoB))
+				break;
+
+			System.out.println("\n-- Turno Equipo B --");
+			for (persona.Personaje p : equipoB) {
+				if (p != null && p.estaVivo()) {
+					p.hacerTurno(equipoA, equipoB);
+					sleep(1700);
 				}
 			}
 
-			// al final de la ronda se procesan estados y se regenera mana
 			System.out.println("\n-- Estados y mana --");
-			procesarEquipo(rebeldes);
-			procesarEquipo(imperio);
+			procesarEquipo(equipoA);
+			procesarEquipo(equipoB);
+			sleep(1700);
 
 			ronda++;
 		}
 
-		// TODO falta que el jugador elija su equipo con scanner
-		// TODO falta resumen final con stats de cada personaje
 		System.out.println("\n=== FIN DEL COMBATE ===");
-		if (hayVivos(rebeldes)) {
-			System.out.println("Ganan los rebeldes en " + (ronda - 1) + " rondas.");
+
+		if (!hayVivos(equipoB) && hayVivos(equipoA))
+			System.out.println("Gana el Equipo A");
+		else if (!hayVivos(equipoA) && hayVivos(equipoB))
+			System.out.println("Gana el Equipo B");
+		else
+			System.out.println("Empate");
+
+		System.out.println("\n--- Equipo A ---");
+		for (persona.Personaje p : equipoA)
+			if (p != null)
+				System.out.println("  " + p.nombre + " | Vida: " + p.vida + "/" + p.vidaMax + " | "
+						+ (p.estaVivo() ? "VIVO" : "ELIMINADO"));
+
+		System.out.println("\n--- Equipo B ---");
+		for (persona.Personaje p : equipoB)
+			if (p != null)
+				System.out.println("  " + p.nombre + " | Vida: " + p.vida + "/" + p.vidaMax + " | "
+						+ (p.estaVivo() ? "VIVO" : "ELIMINADO"));
+
+		sc.close();
+	}
+
+	/**
+	 * Gestiona el turno manual del jugador mostrando las acciones disponibles segun
+	 * el tipo de personaje y pidiendo objetivo si es necesario.
+	 *
+	 * @param p        el personaje que actua
+	 * @param enemigos array del equipo enemigo
+	 * @param aliados  array del equipo aliado
+	 */
+	private static void turnoJugador(persona.Personaje p, persona.Personaje[] enemigos, persona.Personaje[] aliados) {
+
+		System.out.println("\nTurno de " + p.nombre);
+
+		if (p instanceof persona.Jedi) {
+			System.out.println("1) Ataque basico");
+			System.out.println("2) Empujon Fuerza");
+			System.out.println("3) Aplastamiento");
+			System.out.println("4) Telequinesis");
+			int op = leerEntero(1, 4);
+			if (op == 1)
+				p.ataqueBasico(elegirObjetivo(enemigos));
+			else if (op == 2)
+				p.usarEmpujon(enemigos);
+			else if (op == 3)
+				p.usarAplastamiento(elegirObjetivo(enemigos));
+			else
+				p.usarTelequinesis(elegirObjetivo(enemigos));
+
+		} else if (p instanceof persona.Sith) {
+			System.out.println("1) Ataque basico");
+			System.out.println("2) Empujon Fuerza");
+			System.out.println("3) Aplastamiento");
+			System.out.println("4) Estrangulamiento");
+			int op = leerEntero(1, 4);
+			if (op == 1) {
+				persona.Personaje obj = elegirObjetivo(enemigos);
+				if (obj != null)
+					p.ataqueBasico(obj);
+			} else if (op == 2)
+				p.usarEmpujon(enemigos);
+			else if (op == 3) {
+				persona.Personaje obj = elegirObjetivo(enemigos);
+				if (obj != null)
+					p.usarAplastamiento(obj);
+			} else {
+				persona.Personaje obj = elegirObjetivo(enemigos);
+				if (obj != null)
+					p.usarEstrangulamiento(obj);
+			}
+
+		} else if (p instanceof persona.Cazarrecompensas) {
+			System.out.println("1) Ataque basico");
+			System.out.println("2) Quemadura");
+			int op = leerEntero(1, 2);
+			if (op == 1)
+				p.ataqueBasico(elegirObjetivo(enemigos));
+			else
+				p.usarQuemadura(elegirObjetivo(enemigos));
+
+		} else if (p instanceof persona.Sanador) {
+			System.out.println("1) Ataque basico");
+			System.out.println("2) Curacion");
+			System.out.println("3) Renovar");
+			int op = leerEntero(1, 3);
+			if (op == 1)
+				p.ataqueBasico(elegirObjetivo(enemigos));
+			else if (op == 2)
+				p.usarCuracion(elegirObjetivo(aliados));
+			else
+				p.usarRenovar(elegirObjetivo(aliados));
+
 		} else {
-			System.out.println("Gana el imperio en " + (ronda - 1) + " rondas.");
+			System.out.println("1) Ataque basico");
+			System.out.println("2) Granada venenosa");
+			int op = leerEntero(1, 2);
+			if (op == 1)
+				p.ataqueBasico(elegirObjetivo(enemigos));
+			else
+				p.usarVeneno(elegirObjetivo(enemigos));
 		}
 	}
 
-	// comprueba si queda alguien vivo en el equipo
-	public static boolean hayVivos(persona.Personaje[] equipo) {
+	/**
+	 * Muestra la lista de personajes vivos del equipo y devuelve el elegido. No
+	 * permite seleccionar personajes muertos.
+	 *
+	 * @param equipo array del equipo del que se elige objetivo
+	 * @return el personaje vivo seleccionado por el jugador
+	 */
+	private static persona.Personaje elegirObjetivo(persona.Personaje[] equipo) {
+		System.out.println("Elige objetivo:");
 		for (int i = 0; i < equipo.length; i++) {
 			if (equipo[i] != null && equipo[i].estaVivo())
-				return true;
+				System.out.println((i + 1) + ") " + equipo[i].nombre + " (" + equipo[i].vida + " vida)");
+			else
+				System.out.println((i + 1) + ") [ELIMINADO]");
 		}
+		while (true) {
+			int op = leerEntero(1, equipo.length);
+			if (equipo[op - 1] != null && equipo[op - 1].estaVivo())
+				return equipo[op - 1];
+			System.out.println("Ese objetivo no vale.");
+		}
+	}
+
+	/**
+	 * Permite al jugador elegir 3 personajes para su equipo con repeticiones
+	 * permitidas.
+	 *
+	 * @param etiqueta nombre del equipo que se muestra en pantalla
+	 * @return array de 3 personajes creados segun la eleccion del jugador
+	 */
+	private static persona.Personaje[] elegirEquipo(String etiqueta) {
+		persona.Personaje[] equipo = new persona.Personaje[3];
+		int[] repetidos = new int[7];
+		for (int i = 0; i < 3; i++) {
+			System.out.println("\n[" + etiqueta + "] Personaje " + (i + 1));
+			for (int j = 1; j <= 6; j++)
+				System.out.println(j + ") " + menuPersonajes.get(j));
+			int tipo = leerEntero(1, 6);
+			repetidos[tipo]++;
+			String nombre = nombreAutomatico(tipo, repetidos[tipo]);
+			equipo[i] = crearPersonaje(tipo, nombre);
+			System.out.println("Has elegido a " + equipo[i].nombre);
+		}
+		return equipo;
+	}
+
+	/**
+	 * Devuelve el nombre automatico de un personaje segun su tipo y numero de
+	 * repeticion.
+	 *
+	 * @param tipo   tipo de personaje (1-6)
+	 * @param numero numero de veces que se ha repetido ese tipo
+	 * @return nombre asignado al personaje
+	 */
+	private static String nombreAutomatico(int tipo, int numero) {
+		if (tipo == 1)
+			return numero == 1 ? "Yoda" : "Yoda " + numero;
+		if (tipo == 2)
+			return numero == 1 ? "Darth Vader" : "Darth Vader " + numero;
+		if (tipo == 3)
+			return numero == 1 ? "Han Solo" : "Han Solo " + numero;
+		if (tipo == 4)
+			return numero == 1 ? "Stormtrooper" : "Stormtrooper " + numero;
+		if (tipo == 5)
+			return numero == 1 ? "Boba Fett" : "Boba Fett " + numero;
+		if (tipo == 6)
+			return numero == 1 ? "Leia" : "Leia " + numero;
+		return "Personaje";
+	}
+
+	/**
+	 * Crea un personaje del tipo indicado con el nombre dado.
+	 *
+	 * @param tipo   tipo de personaje (1=Jedi, 2=Sith, 3=SoldadoRebelde,
+	 *               4=SoldadoImperial, 5=Cazarrecompensas, 6=Sanador)
+	 * @param nombre nombre del personaje
+	 * @return instancia del personaje creado
+	 */
+	private static persona.Personaje crearPersonaje(int tipo, String nombre) {
+		if (tipo == 1)
+			return new persona.Jedi(nombre);
+		if (tipo == 2)
+			return new persona.Sith(nombre);
+		if (tipo == 3)
+			return new persona.SoldadoRebelde(nombre);
+		if (tipo == 4)
+			return new persona.SoldadoImperial(nombre);
+		if (tipo == 5)
+			return new persona.Cazarrecompensas(nombre);
+		return new persona.Sanador(nombre);
+	}
+
+	/**
+	 * Muestra en consola el estado actual de un personaje. Incluye vida, mana y
+	 * estados activos con sus turnos restantes.
+	 *
+	 * @param p el personaje a mostrar
+	 */
+	private static void mostrarPersonaje(persona.Personaje p) {
+		if (!p.estaVivo()) {
+			System.out.println("  [ELIMINADO] " + p.nombre);
+			return;
+		}
+		System.out.print(
+				"  " + p.nombre + " | Vida: " + p.vida + "/" + p.vidaMax + " | Mana: " + p.mana + "/" + p.manaMax);
+		if (p.numEstados > 0) {
+			System.out.print(" | Estados: ");
+			for (int i = 0; i < p.numEstados; i++) {
+				System.out.print(p.estados[i].nombre + "(" + p.estados[i].turnosRestantes + ")");
+				if (i < p.numEstados - 1)
+					System.out.print(", ");
+			}
+		} else {
+			System.out.print(" | Estados: ninguno");
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Comprueba si algun personaje del equipo sigue vivo.
+	 *
+	 * @param equipo array del equipo a comprobar
+	 * @return true si hay al menos un personaje vivo, false si todos estan
+	 *         eliminados
+	 */
+	public static boolean hayVivos(persona.Personaje[] equipo) {
+		for (persona.Personaje p : equipo)
+			if (p != null && p.estaVivo())
+				return true;
 		return false;
 	}
 
-	// procesa estados y regenera mana de todo el equipo
+	/**
+	 * Procesa los estados activos y regenera el mana de todos los personajes vivos
+	 * del equipo.
+	 *
+	 * @param equipo array del equipo a procesar
+	 */
 	public static void procesarEquipo(persona.Personaje[] equipo) {
-		for (int i = 0; i < equipo.length; i++) {
-			if (equipo[i] != null && equipo[i].estaVivo()) {
-				equipo[i].procesarEstados();
-				equipo[i].regenerarMana();
+		for (persona.Personaje p : equipo)
+			if (p != null && p.estaVivo()) {
+				p.procesarEstados();
+				p.regenerarMana();
 			}
+	}
+
+	/**
+	 * Lee un entero por consola dentro del rango indicado. Repite la lectura si el
+	 * valor no es valido.
+	 *
+	 * @param min valor minimo aceptado
+	 * @param max valor maximo aceptado
+	 * @return entero valido introducido por el jugador
+	 */
+	private static int leerEntero(int min, int max) {
+		while (true) {
+			try {
+				int n = Integer.parseInt(sc.nextLine());
+				if (n >= min && n <= max)
+					return n;
+			} catch (Exception e) {
+			}
+			System.out.print("Numero incorrecto: ");
+		}
+	}
+
+	/**
+	 * Pausa la ejecucion el numero de milisegundos indicado. Se usa para que el
+	 * combate sea mas facil de seguir en consola.
+	 *
+	 * @param ms milisegundos a esperar
+	 */
+	public static void sleep(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
 		}
 	}
 }
